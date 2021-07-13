1c33e02b40e6297fc63e9972175d9908a7dff411
Peter Luna <lunapeter92@gmail.com>
2021-07-13 18:29:48 -0400

    finished lab 02

--- beasts/src/Components/Main.js
+++ beasts/src/Components/Main.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import HornedBeasts from './HornedBeasts';
+import Data from '../data/data.json'
 
 
 class Main extends React.Component{
     render(){
         return(
             <div className="main">
-            <HornedBeasts title="Mike W" imageUrl="https://www.publicdomainpictures.net/pictures/270000/velka/monster-with-one-eye.jpg" description="Crazy Mike" />
-            <HornedBeasts title="Sully" imageUrl="https://cdn.pixabay.com/photo/2016/01/10/14/14/monster-1131847_960_720.png" description="Funny Sully" />
-            <HornedBeasts title="Dwight" imageUrl="https://cdn.pixabay.com/photo/2016/03/22/17/28/transparent-background-1273346_960_720.png" description="Creepy Dwight" />
+             { Data.map((item, idx) => {
+                return <HornedBeasts title={item.title} imageUrl={item.image_url} description={item.description} key={idx}/>
+              })}
+
+
+            
            </div>
         )
     }
